86ac19f733b21098ff99d74f1b1fd9aefb6f20a2
Krishna Raj <reach.krishnaraj@gmail.com>
2025-01-02 08:35:50 -0500

    wss

--- src/main/webapp/app/modules/trade-app/dashboard/TradingDashboardV2.tsx
+++ src/main/webapp/app/modules/trade-app/dashboard/TradingDashboardV2.tsx
@@ -11,7 +11,8 @@ import SignalsTable from './SignalsTable';
 import { useParams } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-const WS_URL = process.env.WEBSOCKET_URL || 'http://localhost:8080/websocket/tracker';
+//const WS_URL = 'http://localhost:8080/websocket/tracker';
+const WS_URL = 'wss://trade-app-production.up.railway.app/websocket/tracker';
 
 const TradingDashboardV2: React.FC = () => {
   const { symbol } = useParams<{ symbol: string }>(); // Extract symbol from URL
