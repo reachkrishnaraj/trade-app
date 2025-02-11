ec39597bff67cfedcc96c19c8998ee2b37aa81ee
Krishna Raj <reach.krishnaraj@gmail.com>
2025-02-11 08:24:19 -0500

    fix tz

--- src/main/java/com/kraj/tradeapp/core/model/CommonUtil.java
+++ src/main/java/com/kraj/tradeapp/core/model/CommonUtil.java
@@ -7,6 +7,12 @@ import java.time.ZonedDateTime;
 public class CommonUtil {
 
     public static LocalDateTime getNYLocalDateTimeNow() {
-        return ZonedDateTime.now().withZoneSameInstant(ZoneId.of("America/New_York")).toLocalDateTime();
+        return ZonedDateTime.now(ZoneId.of("UTC")) // Use UTC as base
+            .withZoneSameInstant(ZoneId.of("America/New_York"))
+            .toLocalDateTime();
+    }
+
+    public static LocalDateTime getUTCNow() {
+        return ZonedDateTime.now(ZoneId.of("UTC")).toLocalDateTime();
     }
 }
